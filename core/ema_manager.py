26008992a5ece7552ede01818d07fe7f984fb48f
Lonely7th <35439521+Lonely7th@users.noreply.github.com>
2018-06-07 23:49:49 +0800

    20180607

--- core/ema_manager.py
+++ core/ema_manager.py
@@ -31,11 +31,29 @@ class EmaManager:
                 if time_cmp(str(date), tk_item["date"]):
                     close_list.append(tk_item["close"])
             # 执行判断条件
-            ema_20 = np.mean(close_list[-20:])
+            ema_20_1 = np.mean(close_list[-20:])
+            ema_20_2 = np.mean(close_list[-21:-1])
+            if close_list[-1] > ema_20_1 and close_list[-2] < ema_20_2:
+                buy_list.append(ticker)
         return buy_list
 
     def get_sell_list(self, date):
-        pass
+        code_list = [x for x in self.db_manager_tk.get_code_list_02()]
+        sell_list = list()
+        for code_item in code_list:
+            ticker = code_item["ticker"]
+            # 获取数据
+            close_list = list()
+            tk_details = self.db_manager_tk.find_one_by_key({"ticker": ticker})
+            for tk_item in [x for x in tk_details["price_list"]]:
+                if time_cmp(str(date), tk_item["date"]):
+                    close_list.append(tk_item["close"])
+            # 执行判断条件
+            ema_10_1 = np.mean(close_list[-10:])
+            ema_10_2 = np.mean(close_list[-11:-1])
+            if close_list[-1] < ema_10_1 and close_list[-2] > ema_10_2:
+                sell_list.append(ticker)
+        return sell_list
 
 
 if __name__ == "__main__":
